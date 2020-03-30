73d8702289bc297a452d499aee96580c891992d0
d-taylor6403 <dannielletaylor.com>
2020-03-30 13:05:50 -0500

    uploading server.js setup

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+//Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+//Create an instance of the express app.
+var app = express();
+
+//Set the port of the application
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
