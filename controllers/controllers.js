88a42b26929e7489c5456091a6c8c5b59eb3bc1c
Dannielle Taylor <dannielletaylor@outlook.com>
2020-03-30 15:37:20 -0500

    adding router.post to controller

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -16,3 +16,9 @@ router.get("/", function(req, res) {
         res.render("inex", hdbrsObj);
     });
 });
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({id: result.insertID});
+    });
+});
