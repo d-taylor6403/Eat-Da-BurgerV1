e0afab76598716fb82f544f18d6ea541c65c620e
Dannielle Taylor <dannielletaylor@outlook.com>
2020-03-30 16:33:38 -0500

    updating connection id object

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,35 +1,31 @@
 //Dependcies
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 //Importing in database model
-var burger = require("../models/burger.js");
+var burger = require('../models/burger.js');
 
 //Creating all routes and setting up logic within those routes where required.
-router.get("/", function(req, res) {
+router.get('/', function(req, res) {
 	burger.selectAll(function(data) {
 		var hdbrsObj = {
 			burgers: data
 		};
 		console.log(hdbrsObj);
-		res.render("inex", hdbrsObj);
+		res.render('inex', hdbrsObj);
 	});
 });
 
-router.post("/api/burgers", function(req, res) {
-	burger.insertOne(
-		["burger_name", "devoured"],
-		[req.body.burger_name, req.body.devoured],
-		function(result) {
-			res.json({ id: result.insertID });
-		}
-	);
+router.post('/api/burgers', function(req, res) {
+	burger.insertOne([ 'burger_name', 'devoured' ], [ req.body.burger_name, req.body.devoured ], function(result) {
+		res.json({ id: result.insertID });
+	});
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-	var condition = " id = " + req.params.id;
+router.put('/api/burgers/:id', function(req, res) {
+	var condition = ' id = ' + req.params.id;
 
-	console.log("condition:", condition);
+	console.log('condition:', condition);
 
 	burger.updateOne(
 		{
@@ -46,10 +42,10 @@ router.put("/api/burgers/:id", function(req, res) {
 	);
 });
 
-router.destroy("/api/burgers/:id", function(req, res) {
-	var condition = "id = " + req.params.id;
+router.delete('/api/burgers/:id', function(req, res) {
+	var condition = 'id = ' + req.params.id;
 
-	console.log("condition:", condition);
+	console.log('condition:', condition);
 
 	burger.deleteOne(condition, function(result) {
 		if (result.changedRows === 0) {
@@ -60,4 +56,3 @@ router.destroy("/api/burgers/:id", function(req, res) {
 });
 
 module.exports = router;
-
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 //Create an instance of the express app.
 var app = express();
@@ -9,18 +9,17 @@ var app = express();
 //Set the port of the application
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-var routes = require("./controllers/burgers_controllers.js");
+var routes = require('./controllers/controllers.js');
 app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+	console.log('Server listening on: http://localhost:' + PORT);
 });
-
