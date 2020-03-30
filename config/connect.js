ac8e6dfefb89f876fc086dde51df77e00e8e77a2
d-taylor6403 <dannielletaylor.com>
2020-03-30 16:33:38 -0500

    updating connection id object

--- config/connect.js
+++ config/connect.js
@@ -2,19 +2,19 @@ var mysql = require('mysql');
 require('dotenv').config();
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: process.env.USER,
-    password: process.env.PASS,
-    database: 'burgers_db'
+	host: 'localhost',
+	port: 3306,
+	user: process.env.USER,
+	password: process.env.PASS,
+	database: 'burgers_db'
 });
 
 connection.connect(function(err) {
-    if (err) {
-        console.log('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadID);
+	if (err) {
+		console.log('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
