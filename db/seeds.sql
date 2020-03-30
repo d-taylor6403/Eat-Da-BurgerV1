f724033950960acea8f7c531646c6f38d0637aee
Dannielle Taylor <dannielletaylor@outlook.com>
2020-03-30 12:35:15 -0500

    uploading initial file structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheesburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Garden Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lamb Burger', false);
