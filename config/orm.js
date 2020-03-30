a5b803ec294626fd1410588db4f76cf75d6cc1c8
d-taylor6403 <dannielletaylor.com>
2020-03-30 13:58:55 -0500

    adding routes to orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,50 @@
-var connection = require('./connection.js');
+var connection = require('./connect.js');
 
 var orm = {
-    
-}
+    selectAll: function(table, cb) {
+        var dbQuery = "SELECT * FROM " + table + ";";
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+
+    insertOne: function(table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO " + table + "(" + cols.toString() + ")" + "VALUES (" + createQmarks(vals.length) + ")";
+
+        console.log(dbQuery);
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+
+    updateOne: function(table, objColVals, condition, cd) {
+        var dbQuery = "UPDATE" + table + "SET" + translateSql(objColVals) + "WHERE" + condition;
+
+        console.log(dbQuery);
+        connection.query(dbQuery, vals,  function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+        
+    },
+
+    deleteOne: function(table, condition, cb) {
+        var dbQuery = "DELETE FROM " + table + "WHERE" + condition;
+
+        console.log(dbQuery);
+        connection.query(dbQuery, vals,  function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+};
