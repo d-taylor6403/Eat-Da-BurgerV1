92ee3e337ccae84aab3cdb7f07d9a875e5152c69
Dannielle Taylor <dannielletaylor@outlook.com>
2020-03-31 19:46:53 -0500

    adding heroku deployment code to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 var mysql = require('mysql');
 require('dotenv').config();
+var connection;
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: process.env.USER,
-	password: process.env.PASS,
-	database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'process.env.USER',
+    password: 'process.env.PASS',
+    database: 'burgers_db',
+  });
+}
 
-connection.connect(function(err) {
-	if (err) {
-		console.log('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
